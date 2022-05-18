36a73e098f1cf3a1446467203399e26006e488e4
Nayef-Abou-Tayoun <nayef.abou.tayoun@ibm.com>
2022-05-18 00:30:58 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ app = Flask(__name__)
 
 conn=ibm_db.connect(os.environ['dbcred'], "", "")
 
-@app.route("/")
+#@app.route("/")
 
-@app.route('/db2')
+@app.route('/')
 def access_db():
-    try:
-        sql = ''' SELECT * FROM JDIRVING.DEMAND '''
-        df = pd.read_sql(sql, conn)
+    #try:
+    sql = ''' SELECT * FROM JDIRVING.DEMAND '''
+    df = pd.read_sql_query(sql, conn)
         #dictionary = ibm_db.fetch_both(stmt)
-        if(df):
-            return "<h1 style='text-align:center;'>Table Values</br></h1>"\
-                    +f"<h2 style='color:blue;text-align:center;'></br>{df['Volume'][0]}</h2>"
-        else:
-            return "<h1 style='color:red;text-align:center;'>Table Values Is Empty"
+        # if(df):
+    return "<h1 style='text-align:center;'>Table Values</br></h1>"\
+        +f"<h2 style='color:blue;text-align:center;'></br>{df.to_html()}</h2>"
+    #     else:
+    #         return "<h1 style='color:red;text-align:center;'>Table Values Is Empty"
             
-    except:
-        return "<h1 style='color:red;text-align:center;'>Table Values Doesn't Exist"
+    # except:
+    #     return "<h1 style='color:red;text-align:center;'>Table Values Doesn't Exist"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
